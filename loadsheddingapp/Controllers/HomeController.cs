ec4607d2ca1f545f1fa4ccc5e122cfeb0012c6e4
Jacus Horne <jacus@bbd.co.za>
2022-07-05 17:58:20 +0200

    added some initial stuff for joke form

--- loadsheddingapp/Controllers/HomeController.cs
+++ loadsheddingapp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace loadsheddingapp.Controllers
             return View();
         }
 
+        public IActionResult CreateJoke()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
